1683c09db89b6b7183f5f0646d30d5e4955506b2
Madeleine Abraham <mads@Madeleines-MacBook-Pro.local>
2023-08-04 17:06:18 -0400

    added global vars

--- assets/script.js
+++ assets/script.js
@@ -2,4 +2,29 @@
 //Once I answer a question, another one shows up automatically
 //The answer is shown correct or incorrect
 //When the timer runs out or questions run out the game is over
-// I am able to see my score and other highscores and record my initials for the h
+// I am able to see my score and other highscores and record my initials for the highscores
+
+// global variables
+const startButton = document.querySelector('start');
+const questionContainer = document.querySelector('question-container');
+const questionElement = document.querySelector('question');
+const choicesList = document.querySelector('choices');
+const endScreen = document.querySelector('end-screen');
+const endResult = document.querySelector('end-result');
+const countdown = document.querySelector('countdown');
+// quiz function
+
+
+
+// start timer function here
+
+
+
+
+// score keeping func here
+
+
+
+
+
+// highscore func here?
