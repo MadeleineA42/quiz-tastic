bb9db16359eb436ef4100924a34233eedd472511
Madeleine Abraham <mads@Madeleines-MacBook-Pro.local>
2023-08-04 15:46:20 -0400

    added checklist to js

---
+++ assets/script.js
@@ -0,0 +1,5 @@
+//Timer starts when I select the start button
+//Once I answer a question, another one shows up automatically
+//The answer is shown correct or incorrect
+//When the timer runs out or questions run out the game is over
+// I am able to see my score and other highscores and record my initials for the h
